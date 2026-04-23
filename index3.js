54d61f3aa55ff594d523dfee5dae3a83acf9dd5f
Victor N <victorkjngo@gmail.com>
2026-04-23 11:35:57 -0700

    Add statements for conversational AI informed by similiarity search rankings

---
+++ index3.js
@@ -0,0 +1,59 @@
+/*
+    Objective: Create a conversational response using OpenAI
+*/
+
+import { openai, supabase } from "./config.js";
+
+// User query about podcasts
+const query = "please suggest some episodes about diving into eating food";
+main(query);
+
+// Bring all function calls together
+async function main(input) {
+  const embedding = await createEmbedding(input);
+  const match = await findNearestMatch(embedding);
+  await getChatCompletion(match, input);
+}
+
+// Create an embedding vector representing the input text
+async function createEmbedding(input) {
+  const embeddingResponse = await openai.embeddings.create({
+    model: "text-embedding-ada-002",
+    input,
+  });
+  return embeddingResponse.data[0].embedding;
+}
+
+// Query Supabase and return a semantically matching text chunk
+async function findNearestMatch(embedding) {
+  const { data } = await supabase.rpc("match_documents", {
+    query_embedding: embedding,
+    match_threshold: 0.5,
+    match_count: 1,
+  });
+  return data[0].content;
+}
+
+// Use OpenAI to make the response conversational
+const chatMessages = [
+  {
+    role: "system",
+    content: `You are an enthusiastic podcast expert who loves recommending podcasts to people. You will be given two pieces of information - some context about podcasts episodes and a question. Your main job is to formulate a short answer to the question using the provided context. If you are unsure and cannot find the answer in the context, say, "Sorry, I don't know the answer." Please do not make up the answer.`,
+  },
+];
+
+async function getChatCompletion(text, query) {
+  chatMessages.push({
+    role: "user",
+    content: `Context: ${text} Question: ${query}`,
+  });
+
+  const response = await openai.chat.completions.create({
+    model: "gpt-4",
+    messages: chatMessages,
+    temperature: 0.5, // balance between creativity and predictability
+    frequency_penalty: 0.5, // Punishes model repetition
+  });
+
+  console.log(response.choices[0].message.content);
+}
