0fb76f69c0537382ef6be321a010e2e6ff70fa9f
Victor N <victorkjngo@gmail.com>
2026-04-24 12:27:53 -0700

    Add file on splitting + vectorizing + storing challenge

---
+++ index4_chunking.js
@@ -0,0 +1,72 @@
+import { openai, supabase } from "./config.js";
+import { RecursiveCharacterTextSplitter } from "@langchain/textsplitters";
+import fs from "fs/promises"
+
+
+// Text Splitter
+// https://docs.langchain.com/oss/javascript/integrations/splitters
+
+/*
+  Challenge: Text Splitters, Embeddings, and Vector Databases!
+    1. Use LangChain to split the content in movies.txt into smaller chunks.
+    2. Use OpenAI's Embedding model to create an embedding for each chunk.
+    3. Insert all text chunks and their corresponding embedding
+       into a Supabase database table.
+ */
+
+/* Split movies.txt into text chunks.
+Return LangChain's "output" – the array of Document objects. */
+
+async function splitDocument(document) {
+  const splitter = new RecursiveCharacterTextSplitter({
+    chunkSize: 100,
+    chunkOverlap: 0,
+  });
+  return splitter.splitText(document);
+}
+
+/* Create an embedding from each text chunk.
+Store all embeddings and corresponding text in Supabase. */
+async function createAndStoreEmbeddings() {
+  // for remote resources
+  // const response = await fetch("movies.txt");
+    // const text = await response.text()
+    try {
+        // Read a local file
+        const text = await fs.readFile("movies.txt", "utf-8");
+        const chunkData = (await splitDocument(text));
+
+        // Get embeddings
+    
+        const chunkEmbeddings = await Promise.all(chunkData.map(async (chunk, i) => {
+            const embedding = await openai.embeddings.create({
+                model: "text-embedding-ada-002",
+                input: chunk,
+            });
+
+            return {
+                content: chunkData[i],
+                embedding: embedding.data[0].embedding,
+            };
+            })
+        )
+    
+        // Store in supabase
+        const { data, error } = await supabase.from('documents').insert(chunkEmbeddings)
+        console.log('data', data)
+        if (error) {
+            console.log('SUPABASE insert failed: ', error)
+        }
+        
+        // For deleting null entries!
+        // const nullEntries = await supabase.from('documents').delete().is('content', null)
+        // console.log(nullEntries)
+    } catch (err) { 
+        throw err
+    } 
+    // No need to close Supabae client
+
+    
+}
+
+createAndStoreEmbeddings()
