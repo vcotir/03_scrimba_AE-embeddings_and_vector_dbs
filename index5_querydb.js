9747e4a099621c14baf9bd6477eb4d69c5a47c7d
Victor N <victorkjngo@gmail.com>
2026-04-24 18:03:09 -0700

    Add new file for new semantic search + chat bot submodule

---
+++ index5_querydb.js
@@ -0,0 +1,72 @@
+/*
+    Objective: Creates query emedding, do semantic search against DB, leverage compleitions and semantic results to get suggestions for movies
+*/
+import { openai, supabase } from "./config.js";
+
+// Query about our movie data
+const query = "I feel like an adrenaline rush";
+main(query);
+
+async function main(input) {
+    try {
+        const embedding = await createEmbedding(input);
+        const match = await findNearestMatch(embedding);
+        await getChatCompletion(match, input);
+    } catch (err) { 
+        console.error('Issue in main function: ', err.message)
+        throw new Error(err)
+    }
+}
+
+// Create an embedding vector representing the query
+async function createEmbedding(input) {
+  const embeddingResponse = await openai.embeddings.create({
+    model: "text-embedding-ada-002",
+    input,
+  });
+  return embeddingResponse.data[0].embedding;
+}
+
+/*
+  Challenge: Return and manage multiple matches
+    - Return at least 3 matches from the database table
+    - Combine all of the matching text into 1 string
+*/
+
+// Query Supabase and return a semantically matching text chunk
+async function findNearestMatch(embedding) {
+  const { data } = await supabase.rpc("match_movies", {
+    query_embedding: embedding,
+    match_threshold: 0.5,
+    match_count: 4,
+  });
+    const content = data.map(val => { 
+        return val.content
+    })
+    console.log(content)
+
+    return content.join('\n')
+}
+
+// Use OpenAI to make the response conversational
+const chatMessages = [
+  {
+    role: "system",
+    content: `You are an enthusiastic movie expert who loves recommending movies to people. You will be given two pieces of information - some context about movies and a question. Your main job is to formulate a short answer to the question using the provided context. If you are unsure and cannot find the answer in the context, say, "Sorry, I don't know the answer." Please do not make up the answer.`,
+  },
+];
+
+async function getChatCompletion(text, query) {
+  chatMessages.push({
+    role: "user",
+    content: `Context: ${text} Question: ${query}`,
+  });
+
+  const response = await openai.chat.completions.create({
+    model: "gpt-4",
+    messages: chatMessages,
+    temperature: 0.5,
+    frequency_penalty: 0.5,
+  });
+  console.log(response.choices[0].message.content);
+}
